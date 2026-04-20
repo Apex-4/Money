eda9c289695405935e069d3402535c75200b40cb
Apex-4 <143663199+Apex-4@users.noreply.github.com>
2026-04-20 13:59:04 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+app.get("/api/healthz", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
